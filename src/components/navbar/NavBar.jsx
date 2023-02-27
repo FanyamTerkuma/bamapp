12803af988f9766a664d79b8d8386ff565210046
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-02-27 18:55:40 +0100

    Added links to the respective pages.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 function NavBar() {
-	return <div>NavBar</div>;
+    return (
+        <div className="shadow-xl">
+            <NavLink className="flex justify-evenly py-4 uppercase cursor-pointer">
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/users">Users</Link>
+                <Link to="/savings">Savings</Link>
+                <Link to="/loans">Loans</Link>
+                <Link to="/repayments">Repayments</Link>
+                <Link to="/summary">Summary</Link>
+            </NavLink>
+        </div>
+    );
 }
 
 export default NavBar;
