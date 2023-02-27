935764eee1a64d9425ca136f4f872af807203b7f
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-02-27 18:55:03 +0100

    Added browser routes and configured paths to various components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar/NavBar';
+import { Home, Loan, Login, Repayment, Savings, Summary } from './pages';
+import Users from './pages/users/Users';
 
 function App() {
-	return <NavBar />;
+    return (
+        <div>
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/loans" element={<Loan />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/repayments" element={<Repayment />} />
+                    <Route path="/savings" element={<Savings />} />
+                    <Route path="/users" element={<Users />} />
+                    <Route path="/summary" element={<Summary />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
