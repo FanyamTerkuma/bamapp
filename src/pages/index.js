ca9a9e976734fb4ca8c75428f25395106624ae68
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-03-02 13:32:58 +0100

    added imports for the newly added components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,8 @@ import { default as Repayment } from './repayment/Repayment';
 import { default as Savings } from './savings/Savings';
 import { default as Summary } from './summary/Summary';
 import { default as User } from './users/Users';
+import { default as AddUser } from './users/AddUser';
+import { default as AddSavings } from './savings/AddSavings';
+import { default as AddLoan } from './loans/AddLoan';
 
-export { Home, Loan, Login, Repayment, Savings, Summary, User };
+export { Home, Loan, Login, Repayment, Savings, Summary, User, AddUser, AddSavings, AddLoan };
