c68ae11cc1200f752575dad3bc259d0dc20accde
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-03-02 13:35:56 +0100

    added the necessary components to the loan page

--- src/pages/loans/Loan.jsx
+++ src/pages/loans/Loan.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Button from '../../components/button/Button';
+import Table from '../../components/table/Table';
+const tableheaders = ['ID', 'Amount loaned', 'Interest', 'Activate', 'Balance', 'Purpose', 'Svn', 'Action'];
+const tableData = [
+    {
+        id: 'id2923',
+        amount_loaned: 230000,
+        interest: 'true',
+        activate: 'true',
+        balance: 60000,
+        purpose: 'School fees',
+        svn: 'NCS1283'
+    },
+    {
+        id: 'id2923',
+        amount_loaned: 230000,
+        interest: 'true',
+        activate: 'true',
+        balance: 60000,
+        purpose: 'School fees',
+        svn: 'NCS1283'
+    }
+];
 function Loan() {
-  return <div>Loan</div>;
+    return (
+        <div>
+            <Link to="/addloan" className="flex w-full justify-end px-1 py-2 ">
+                <Button buttonName="create new loan" />
+            </Link>
+            <Table tableHead={tableheaders} tableData={tableData} />
+        </div>
+    );
 }
 
 export default Loan;
