89a9f12e644d7d18a4f488faeb5db78938c803de
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-03-02 13:40:49 +0100

    Added the necessary components to the  Savings page.

--- src/pages/savings/Savings.jsx
+++ src/pages/savings/Savings.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Button from '../../components/button/Button';
+import Table from '../../components/table/Table';
+const tableHeaders = ['Id', 'Amount', 'Svn', 'Action'];
+const savings = [
+    {
+        id: '12023',
+        amount: 2000,
+        svn: 'NCS3202'
+    },
+    {
+        id: '12024',
+        amount: 200,
+        svn: 'NCS3202'
+    }
+];
 function Savings() {
-  return <div>Savings</div>;
+    return (
+        <div>
+            <Link to="/addsavings" className="flex justify-end px-1 py-2">
+                <Button buttonName="create new savings" />
+            </Link>
+            <Table tableHead={tableHeaders} tableData={savings} />
+        </div>
+    );
 }
 
 export default Savings;
