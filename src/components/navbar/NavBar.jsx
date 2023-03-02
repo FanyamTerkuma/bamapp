f32bd89002bed0f7984d86d9598fc695cf18782c
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-03-02 13:32:22 +0100

    Removed link and used navlink only.Also made it dynamic.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
+const navLinks = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'Login',
+        path: '/login'
+    },
+    {
+        name: 'USers',
+        path: '/users'
+    },
+    {
+        name: 'Savings',
+        path: '/savings'
+    },
+    {
+        name: 'Loans',
+        path: '/loans'
+    },
+    {
+        name: 'Repayments',
+        path: '/repayments'
+    },
+    {
+        name: 'Summary',
+        path: '/summary'
+    }
+];
 function NavBar() {
     return (
-        <div className="shadow-xl">
-            <NavLink className="flex justify-evenly py-4 uppercase cursor-pointer">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/savings">Savings</Link>
-                <Link to="/loans">Loans</Link>
-                <Link to="/repayments">Repayments</Link>
-                <Link to="/summary">Summary</Link>
-            </NavLink>
+        <div className="sticky top-0 z-20 flex w-full cursor-pointer flex-col items-center justify-between  px-2 py-4 text-xl font-light uppercase tracking-[0.07em] shadow-xl backdrop-blur-xl sm:flex-row">
+            <span className="bg-blue-400 p-2 tracking-tighter ">icon and name</span>
+            {navLinks.map((link, id) => (
+                <NavLink key={id} to={link.path} className={({ isActive }) => (isActive ? 'skew-y-2  border-b-2 border-blue-300' : '')} end>
+                    {link.name}
+                </NavLink>
+            ))}
         </div>
     );
 }
