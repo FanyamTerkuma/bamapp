2a52fe0f5284150577c7e59e648279ed54283cb8
FanyamTerkuma <89303855+FanyamTerkuma@users.noreply.github.com>
2023-03-02 13:28:38 +0100

    Added routes to newly added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar/NavBar';
-import { Home, Loan, Login, Repayment, Savings, Summary } from './pages';
+import { AddLoan, AddSavings, AddUser, Home, Loan, Login, Repayment, Savings, Summary } from './pages';
 import Users from './pages/users/Users';
 
 function App() {
@@ -11,10 +11,13 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/loans" element={<Loan />} />
+                    <Route path="/addloan" element={<AddLoan />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/repayments" element={<Repayment />} />
                     <Route path="/savings" element={<Savings />} />
+                    <Route path="/addsavings" element={<AddSavings />} />
                     <Route path="/users" element={<Users />} />
+                    <Route path="/adduser" element={<AddUser />} />
                     <Route path="/summary" element={<Summary />} />
                 </Routes>
             </BrowserRouter>
